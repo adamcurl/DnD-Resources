e0dfe0d6f05985122d0cacc9101cd0b3acaef549
adamcurl <adamcurl6@gmail.com>
2019-09-28 16:18:44 -0500

    init

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Adam's DnD Archives</title>
+  </head>
+<header>
+<nav>
+  <a href="/sounds/">Music & Sound Effects</a> |
+  <a href="/items/">Items</a> |
+  <a href="/maps/">Maps</a> |
+  <a href="/scenarios/">Scenarios</a>
+</nav>
+</header>
+<body>
