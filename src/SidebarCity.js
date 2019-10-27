7b465d96b49ea04614d6e293fbc0b8c520696068
Adam 👾 <adamcurl6@gmail.com>
2019-10-26 23:34:49 -0500

    📦  NEW: adds mouseover, sidebar for map

---
+++ src/SidebarCity.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SidebarCity extends Component {
+  render() {
+    return <>{this.props.city}</>;
+  }
+}
+
+export default SidebarCity;
---
+++ src/SidebarCountry.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SidebarCountry extends Component {
+  render() {
+    return <>{this.props.country}</>;
+  }
+}
+
+export default SidebarCountry;
