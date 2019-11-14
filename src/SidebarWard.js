1049986d58666e4c873c97df21e0f01b6e5f2115
Adam 👾 <adamcurl6@gmail.com>
2019-11-13 22:38:24 -0600

    ✅  TEST: sets up printInfo() functionality

--- src/SidebarWard.js
+++ src/SidebarWard.js
@@ -5,7 +5,8 @@ class SidebarWard extends Component {
     super(props);
 
     this.state = {
-      districts: []
+      districts: [],
+      general: {}
     };
   }
 
@@ -21,13 +22,58 @@ class SidebarWard extends Component {
 
   getWardInfo = () => {
     const c = require(`./assets/sharn/${this.props.ward}`);
-
     // get list of districts
     let districtArr = [];
-    c.districts.map(district => districtArr.push(district.districtName));
-    this.setState({ districts: districtArr });
+    if (c.districts) {
+      c.districts.map(district => districtArr.push(district));
+      this.setState({ districts: districtArr });
+    }
+    // get general info
+    if (c.other) this.setState({ general: c.other });
 
     console.log(c);
+  };
+
+  printInfo = obj => {
+    let html = "";
+
+    if (obj.shops && obj.shops.length) {
+      //
+    }
+    if (obj.taverns && obj.taverns.length) {
+      //
+    }
+    if (obj.inns && obj.inns.length) {
+      //
+    }
+    if (obj.entertainment && obj.entertainment.length) {
+      //
+    }
+    if (obj.temples && obj.temples.length) {
+      //
+    }
+    if (obj.government && obj.government.length) {
+      //
+    }
+    if (obj.residential && obj.residential.length) {
+      //
+    }
+    if (obj.knowledge && obj.knowledge.length) {
+      //
+    }
+    if (obj.transportation && obj.transportation.length) {
+      //
+    }
+    if (obj.restaurants && obj.restaurants.length) {
+      //
+    }
+    if (obj.industry && obj.industry.length) {
+      //
+    }
+    if (obj.illegal && obj.illegal.length) {
+      //
+    }
+    return { __html: "" };
   };
 
   render() {
@@ -35,8 +81,15 @@ class SidebarWard extends Component {
       <div className="sidebar_inner">
         <h1>Districts</h1>
         {this.state.districts.map(district => (
-          <h2 key={district}>{district}</h2>
+          <div key={district.districtName}>
+            <h2>{district.districtName}</h2>
+            <p>{district.description}</p>
+            <div dangerouslySetInnerHTML={this.printInfo(district)} />
+          </div>
         ))}
+        <h1>General</h1>
+        <p>{this.state.general.description}</p>
+        <div dangerouslySetInnerHTML={this.printInfo(this.state.general)} />
       </div>
     );
   }
